6f167f4bd8fba707793fe4007a8f847055ab6b1b
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2022-08-22 11:54:46 +0200

    Using the State Hook

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,17 +3,23 @@ import './TodoSearch.css';
 
 function TodoSearch() {
 
+    //       variable       funcion
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
-    return(
+    return[
         <input 
         className="TodoSearch"  
-        onChange={onSearchValueChange}
         placeholder="Compra ropa"
-        />
-    );
+        value={searchValue}
+        onChange={onSearchValueChange}
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
