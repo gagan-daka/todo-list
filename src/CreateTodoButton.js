a4d17efee5ad134eacb9da82d149692ce79db0e9
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2022-08-22 11:20:06 +0200

    events in react

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,9 +1,27 @@
 import React from "react";
 import './CreateTodoButton.css'
+
 function CreateTodoButton() {
+
+    //sin parametros
+    const onClickButtonWithOutParameter = () => {
+        alert('Aqui debería abrir un modal')
+    }
+    //con parametros
+    const onClickButtonWithParameter = (msg) => {
+        alert(msg)
+    }
+
     return(
-        <button className="CreateTodoButton">+</button>
+        <button 
+        className="CreateTodoButton"
+        //onClick={onClickButtonWithOutParameter}
+        onClick={() => onClickButtonWithParameter('Aqui debería abrir un modal')}
+        >
+            +
+        </button>
     );
+
 }
 
 export { CreateTodoButton };
