dc4e3a1dcc917beac78380c7b7e2c8a07eb073f8
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2022-09-26 13:50:17 +0200

    Files and folders structure

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,5 @@
 import React from "react";
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch";
-import { CreateTodoButton } from "./CreateTodoButton";
-import { TodoItem } from "./TodoItem";
-import { TodoList } from "./TodoList";
-// import './App.css';
+import { AppUI } from "./AppUI";
 
 const defaultTodos = [
   {text: 'Comprar ropa', completed: true},
@@ -50,31 +45,15 @@ function App(props) {
   }
 
   return (
-    <React.Fragment>
-      <TodoCounter
-      total={totalTodos}
-      completed={completedTodos}
-      />
-
-      <TodoSearch 
-      searchValue={searchValue}
+    <AppUI
+      totalTodos={totalTodos} 
+      completedTodos={completedTodos}
+      searchValue={searchValue} 
       setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </React.Fragment>
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
